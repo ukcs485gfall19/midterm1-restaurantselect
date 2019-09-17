2c5afb7f048ce2e4f6dc26ea633001cc0c8973ce
Jon Moore <jdmo242@uky.edu>
2019-09-17 18:09:04 -0400

    Initial Commit

---
+++ Midterm1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Midterm1
+//
+//  Created by Jonathan Moore on 9/17/19.
+//  Copyright © 2019 Jonathan Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
