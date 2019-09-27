da7daef92a2efe40b98683e7082072f128eccc46
Jon Moore <jdmo242@uky.edu>
2019-09-26 22:12:43 -0400

    Added init() into LoacationStorage

--- Midterm1/AppDelegate.swift
+++ Midterm1/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreLocation
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        let center = UNUserNotificationCenter.current()
+        let locationManager = CLLocationManager()
+
         return true
     }
 
--- Midterm1/LocationStorage.swift
+++ Midterm1/LocationStorage.swift
@@ -5,7 +5,21 @@
 //  Created by Jonathan Moore on 9/24/19.
 //  Copyright © 2019 Jonathan Moore. All rights reserved.
 //
+import Foundation
+import CoreLocation
 
 class LocationStorage {
+    private let fileManager: FileManager
+    private let documentsURL: URL
+    private var locations: [Location]
     
+    init(){
+        //Set up the fileManager to be equal to the shared FileManager object
+        fileManager = FileManager.default
+        //Initilation the URL for where to store the location data
+        //Functions defines the file storage to be in the user's home directory
+        //Also, disables error propagation on the function.
+        documentsURL = try! fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        locations = []
+    }
 }
