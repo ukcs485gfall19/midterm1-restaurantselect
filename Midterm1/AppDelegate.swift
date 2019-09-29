46f20d7ae2d5f5f43629c310d9ce05918a7c246d
Oluwafeyisayo Afolabi <feyiafolabi1@hotmail.com>
2019-09-29 12:48:37 -0400

    fixed location permission banner, all done up until faking data section

--- Midterm1/AppDelegate.swift
+++ Midterm1/AppDelegate.swift
@@ -15,20 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    static let geoCoder = CLGeocoder()
+    let center = UNUserNotificationCenter.current()
+    let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let center = UNUserNotificationCenter.current()
-        let locationManager = CLLocationManager()
         
         center.requestAuthorization(options: [.alert, .sound]) { granted, error in
-        }// having issues with making banner not dissapear immediately after showing
-
-        locationManager.requestAlwaysAuthorization() // having issues with making banner not dissapear immediately after showing
+        }
+        // Asking the user for notification permission, still not showing up
+        locationManager.requestAlwaysAuthorization()
+        // Asking the user for location use permission
         locationManager.startMonitoringVisits()
         locationManager.delegate = self
-        
-        
         return true
     }
 
@@ -63,17 +63,37 @@ extension AppDelegate: CLLocationManagerDelegate {
         let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
         
         // Get location description
+        AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
+            if let place = placemarks?.first {
+                let description = "\(place)"
+                self.newVisitReceived(visit, description: description)
+            }
+        }
+
     }
     
     func newVisitReceived(_ visit: CLVisit, description: String) {
         let location = Location(visit: visit, descriptionString: description)
         
-        // need to work on where to add location init
-        
-        
         // Save location to disk
+        
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Hey there 😃! You have a new Journal entry 📌"
+        content.body = location.description
+        content.sound = .default
+        
+      
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+        
+        
+        center.add(request, withCompletionHandler: nil)
+
+        
     }
     
 }
+
 
 
